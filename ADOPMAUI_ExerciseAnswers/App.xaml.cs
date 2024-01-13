c24d8ed6abb2bf79bc2de583a2bec200db6a0614
Martin Lenart <martin_lenart@icloud.com>
2024-01-13 16:23:44 +0100

    Add project files.

---
+++ ADOPMAUI_ExerciseAnswers/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ADOPMAUI_Exercise.Globals;
+using ADOPMAUI_Exercise.Models;
+
+namespace ADOPMAUI_Exercise;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+    protected override void OnStart()
+    {
+        //Demonstrate State management
+        //using Global variables
+
+        //Some Friends with Favorite color Blue
+        Global.Data.Friends = Friend.Factory.CreateRandom(100)
+            .Where(f => (f.FavoriteColor.FriendlyName.Contains("Blue"))).ToList();
+        
+        //Some Cities in Africa
+        Global.Data.Cities = City.List.Where(c => c.Continent == "Africa").ToList();
+
+        //All Named Colors with a strong Red component
+        Global.Data.NamedColors = NamedColor.All.Where(nc => (nc.FriendlyName.Contains("Red"))).ToList();
+    }
+}
---
+++ ADOPMAUI_ExerciseAnswers/Views/Lesson04/ListViewExercise1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using ADOPMAUI_Exercise.Models;
+
+namespace ADOPMAUI_Exercise.Views.Lesson04
+{
+    public partial class ListViewExercise1 : ContentPage
+    {
+        public ListViewExercise1()
+        {
+            InitializeComponent();
+            DataModelList.ItemsSource = CityPopulation.List;
+        }
+
+        async void ListViewItemTapped (object sender, ItemTappedEventArgs e)
+        {
+            CityPopulation item = (CityPopulation)e.Item;
+            await DisplayAlert("Tapped", item.ToString(), "OK");
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
+
