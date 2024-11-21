844731a0f1f56d78866ef20ada9cb2c20d0f5123
Martin <martin_lenart@icloud.com>
2024-11-21 12:48:27 +0900

    initial commit

---
+++ ADOPMAUI_ExerciseAnswers/AppShell.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ADOPMAUI_Exercise.Models;
+using ADOPMAUI_Exercise.Views.Lesson05;
+using Microsoft.Maui;
+
+namespace ADOPMAUI_Exercise;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+
+        //Exercise ShellMenu: Adding an item to a Shell Menu in C#
+        var NewsPapers = NewsSource.All;
+
+        Tab aTab;
+        foreach (var item in NewsPapers)
+        {
+            //Create a Tab, set its relevant properties
+            //Create a ShellContent , set its relevant properties
+            //Add the ShellContent into the Tab
+
+            //Add the Tab into lesson5news2 flyout item from the Xaml page
+            //Your code
+            
+            
+            aTab = new Tab { Title = item.Name };
+            aTab.Items.Add(new ShellContent
+            {
+                Title = item.Name,
+                Route = item.Name.ToLower(),
+                ContentTemplate = new DataTemplate(() => new WebViewExplore(item))
+            });
+            lesson5news2.Items.Add(aTab);
+            
+        }
+    }
+}
---
+++ ADOPMAUI_ExerciseAnswers/Views/Lesson04/ListViewExercise2.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ADOPMAUI_Exercise.Models;
+
+namespace ADOPMAUI_Exercise.Views.Lesson04
+{
+    public partial class ListViewExercise2 : ContentPage
+    {
+        public ListViewExercise2()
+        {
+            InitializeComponent();
+            ImageList.ItemsSource = CityPicture.List;
+        }
+
+        private async void ImageList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            CityPicture item = (CityPicture)e.Item;
+            await DisplayAlert("Tapped", item.ToString(), "OK");
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
+
